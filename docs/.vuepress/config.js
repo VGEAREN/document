7c559ecf76dc570b7b37936b1b968599cb7e335c
vgearen <vgearen@gmail.com>
2022-03-04 16:42:35 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // site config
+    lang: 'en-US',
+    title: 'Vgearen\'s Document',
+    description: '温故知新',
+
+    // theme and its config
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: '😃',
+    },
+}
