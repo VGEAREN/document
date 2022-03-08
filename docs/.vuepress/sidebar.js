14f5c60d762627eb2da7eda84a675ab5753d6e1e
vgearen <vgearen@gmail.com>
2022-03-08 14:34:26 +0800

    add content

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -6,13 +6,33 @@ module.exports = defineSidebarConfig({
             prefix: "java/",
             text: "Java",
             collapsable: true,
-            children: ["java-base.md", "springboot.md"],
+            children: [
+                {
+                    prefix: "jvm/",
+                    text: "JVM虚拟机",
+                    collapsable: true,
+                    children: [
+                        "architecture.md",
+                        "classloader.md",
+                        "runtime_area.md",
+                        "gc.md",
+                        "dispatcher.md",
+                        "string-constant-pool.md",
+                        "jvm-object-lifecycle.md"
+                    ]
+                },
+                "java-base.md",
+                "java-question.md",
+                "springboot.md"],
         },
         {
             prefix: "computer_network/",
             text: "计算机网络",
             collapsable: true,
-            children: ["http.md","https.md","websocket.md"],
+            children: [
+                "http.md",
+                "https.md",
+                "websocket.md"],
         },
         {
             prefix: "mysql/",
@@ -23,7 +43,10 @@ module.exports = defineSidebarConfig({
                     prefix: "innodb/",
                     text: "InnoDB",
                     collapsable: true,
-                    children: ["innodb_index.md","innodb_concurrency_control.md","innodb_transaction.md"],
+                    children: [
+                        "innodb_index.md",
+                        "innodb_concurrency_control.md",
+                        "innodb_transaction.md"],
                 },
                 "mysql_cluster.md"
             ],
