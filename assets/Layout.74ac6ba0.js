85115017365d76e2e1a754f374091f423553ff0f
VGEAREN <VGEAREN@users.noreply.github.com>
2022-03-09 06:47:28 +0000

    deploy: 10e4e745d3998655546569b20b99660368ac4406

---
+++ assets/Layout.74ac6ba0.js
@@ -0,0 +1,1 @@
+import{g as r,m,j as a,r as e,n,p as s}from"./app.5cf4af04.js";import{S as p}from"./SkipLink.404ee5ba.js";var i=r({name:"Layout",setup(){const o=s(),t=m();return()=>[a(p),a(e("CommonWrapper"),()=>t.value.home?a(e("HomePage")):a(n,()=>a(e("NormalPage"),{key:o.value.path})))]}});export{i as default};
