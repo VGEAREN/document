7830db75b2a186f4c696b6120ce10a1a8ed5bd4f
VGEAREN <VGEAREN@users.noreply.github.com>
2022-03-21 08:40:24 +0000

    deploy: f3ff1675ab088d66082a141a31126371e7d31324

---
+++ assets/mysql_remark.html.e2be2a6f.js
@@ -0,0 +1,24 @@
+const t={key:"v-1157cdf6",path:"/mysql/mysql_remark.html",title:"Mysql\u5907\u5FD8",lang:"zh-CN",frontmatter:{title:"Mysql\u5907\u5FD8",date:"2021-10-20T11:27:57.000Z",category:["\u5F00\u53D1","Mysql"],toc:!0,tag:["Mysql"]},excerpt:`<h3 id="mysql\u6743\u9650" tabindex="-1"><a class="header-anchor" href="#mysql\u6743\u9650" aria-hidden="true">#</a> Mysql\u6743\u9650</h3>
+<table>
+<thead>
+<tr>
+<th style="text-align:left">\u6743\u9650\u5206\u5E03</th>
+<th style="text-align:left">\u53EF\u8BBE\u7F6E\u6743\u9650</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left">\u8868\u6743\u9650</td>
+<td style="text-align:left">'Select', 'Insert', 'Update', 'Delete', 'Create', 'Drop', 'Grant', 'References', 'Index', 'Alter'</td>
+</tr>
+<tr>
+<td style="text-align:left">\u5217\u6743\u9650</td>
+<td style="text-align:left">'Select', 'Insert', 'Update', 'References'</td>
+</tr>
+<tr>
+<td style="text-align:left">\u8FC7\u7A0B\u6743\u9650</td>
+<td style="text-align:left">'Execute', 'Alter Routine', 'Grant'</td>
+</tr>
+</tbody>
+</table>
+`,headers:[{level:3,title:"Mysql\u6743\u9650",slug:"mysql\u6743\u9650",children:[]}],git:{createdTime:1647851947e3,updatedTime:1647851947e3,contributors:[{name:"vgearen",email:"vgearen@gmail.com",commits:1}]},readingTime:{minutes:1.42,words:427},filePathRelative:"mysql/mysql_remark.md"};export{t as data};
