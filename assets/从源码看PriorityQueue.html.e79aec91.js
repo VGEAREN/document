2d8c6c869a9cb3e3dd545be949ae83de9ffc61d9
VGEAREN <VGEAREN@users.noreply.github.com>
2022-06-08 08:53:05 +0000

    deploy: d944a161b277690b4051e25788b5c88730acfd3a

--- assets/从源码看PriorityQueue.html.e79aec91.js
+++
@@ -1,9 +0,0 @@
-const e={key:"v-18b4dd5e",path:"/sroucecode/%E4%BB%8E%E6%BA%90%E7%A0%81%E7%9C%8BPriorityQueue.html",title:"\u4ECE\u6E90\u7801\u770BPriorityQueue",lang:"zh-CN",frontmatter:{title:"\u4ECE\u6E90\u7801\u770BPriorityQueue",date:"2021-12-22T09:01:39.000Z",category:["\u5F00\u53D1","Java","\u6E90\u7801"],tag:["Java","\u6E90\u7801"]},excerpt:`<h3 id="add-offer" tabindex="-1"><a class="header-anchor" href="#add-offer" aria-hidden="true">#</a> add/offer</h3>
-<blockquote>
-<p>java.util.PriorityQueue#add</p>
-<p>\u5C06\u6307\u5B9A\u5143\u7D20\u63D2\u5165\u6B64\u4F18\u5148\u7EA7\u961F\u5217\u3002
-\u8FD4\u56DE\uFF1Atrue \uFF08\u7531Collection.add\u6307\u5B9A\uFF09
-\u629B\u51FA\uFF1AClassCastException \u2013 \u5982\u679C\u6839\u636E\u4F18\u5148\u7EA7\u961F\u5217\u7684\u987A\u5E8F\u65E0\u6CD5\u5C06\u6307\u5B9A\u5143\u7D20\u4E0E\u5F53\u524D\u5728\u6B64\u4F18\u5148\u7EA7\u961F\u5217\u4E2D\u7684\u5143\u7D20\u8FDB\u884C\u6BD4\u8F83
-NullPointerException \u2013 \u5982\u679C\u6307\u5B9A\u7684\u5143\u7D20\u4E3A\u7A7A</p>
-</blockquote>
-`,headers:[{level:3,title:"add/offer",slug:"add-offer",children:[]},{level:3,title:"poll",slug:"poll",children:[]}],git:{createdTime:1654675113e3,updatedTime:1654675113e3,contributors:[{name:"vgearen",email:"vgearen@gmail.com",commits:1}]},readingTime:{minutes:1.81,words:543},filePathRelative:"sroucecode/\u4ECE\u6E90\u7801\u770BPriorityQueue.md"};export{e as data};
